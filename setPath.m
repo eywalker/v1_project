61104c5445a33c7f04aff3a16722ba1383e0858a
Edgar Y. Walker <edgar.walker@gmail.com>
2015-02-09 23:16:15 -0600

    Reoganize files into subfolders

--- setPath.m
+++ setPath.m
@@ -3,6 +3,8 @@ function setPath
     base = fileparts(mfilename('fullpath'));
     addpath(base)
     addpath(fullfile(base, 'analysis'))
+    addpath(fullfile(base, 'schema'))
+    addpath(fullfile(base, 'misc_code'))
 
 end
 
