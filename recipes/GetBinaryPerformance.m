ae047af2df3d583c8d3aeb61a2ab5c7ef3a01845
Edgar Y. Walker <edgar.walker@gmail.com>
2016-02-10 18:39:41 -0600

    Update binary output analysis

--- recipes/GetBinaryPerformance.m
+++ recipes/GetBinaryPerformance.m
@@ -1,3 +1,2 @@
-parpopulate(cd_analysis.BinaryReadout, 'lc_trainset_owner like "%ContrastSessionData%" and lc_id = 1')
-
-parpopulate(cd_analysis.BinaryReadout, 'lc_trainset_owner like "%ContrastSessionData%" and lc_id = 7')
+parpopulate(cd_analysis.BinaryReadout, 'lc_trainset_owner like "%ContrastSessionData%"');
+%parpopulate(cd_analysis.BinaryReadout, 'lc_trainset_owner like "%ContrastSessionData%" and lc_id = 7')
