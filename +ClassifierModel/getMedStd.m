9262cfd94cb63ec67e3f5be56852cd74097553a2
Edgar Y. Walker <edgar.walker@gmail.com>
2016-12-05 16:52:11 -0600

    Fix bug in median computation when dealing with nonvector input

--- +ClassifierModel/getMedStd.m
+++ +ClassifierModel/getMedStd.m
@@ -3,7 +3,7 @@ function [med,sigma]=getMedStd(s, L)
     s = s(:);
     s_mu=sum(bsxfun(@times,L,s))./sum(L);
     sigma=sqrt(sum(bsxfun(@times,L,s.^2))./sum(L)-s_mu.^2);
-    [~, pos] = max(diff(cumsum(L) ./ sum(L) > 0.5));
+    [~, pos] = max(diff(bsxfun(@rdivide, cumsum(L), sum(L)) > 0.5));
     pos = pos + 1;
     med = s(pos);
 end
