81ee59b1531a62dce3eb1ff56078bb3ff34f310d
Edgar Y. Walker <edgar.walker@gmail.com>
2016-12-06 16:32:48 -0600

    Get BinaryReadout on Simulated results

--- recipes/GetBinaryPerformance.m
+++ recipes/GetBinaryPerformance.m
@@ -1,2 +1,2 @@
-parpopulate(cd_analysis.BinaryReadout, 'lc_trainset_owner like "%ContrastSessionData%"');
+parpopulate(cd_analysis.BinaryReadout, 'lc_trainset_owner like "%Simulated%"');
 %parpopulate(cd_analysis.BinaryReadout, 'lc_trainset_owner like "%ContrastSessionData%" and lc_id = 7')
