c63ae8b9d82df68c32c7def043d800026683f452
Edgar Y. Walker <edgar.walker@gmail.com>
2016-12-08 18:20:51 -0600

    Add behavioral simulation

---
+++ recipes/RunBehaviorSimulation.m
@@ -0,0 +1,26 @@
+%% Train models on simulated behavioral data
+
+% prepare simulated behavioral responses
+parpopulate(cd_dataset.SimulatedBehavior);
+
+% register SimlatedResponses as decoder trainset
+parpopulate(cd_decoder.DecoderTrainSets, 'dec_trainset_owner = "cd_dataset.SimulatedBehavior"');
+
+% register SimulatedBehavior as LC trainset
+parpopulate(cd_lc.LCTrainSets, 'lc_trainset_owner = "cd_dataset.SimulatedBehavior"');
+
+% regiser pairing between Simulated Behavior decoder trainset and
+% Simulated Behavior LC trainset
+dataset = cd_dataset.DataSets & cd_dataset.SimulatedBehavior;
+rel = pro(cd_decoder.DecoderTrainSets * dataset) * ...
+    pro(cd_lc.LCTrainSets * dataset);
+registerPair(cd_lc.LCTrainSetPairs, rel);
+
+% now finally get onto training decoders and then LC models
+% train all decoders
+parpopulate(cd_decoder.TrainedDecoder, 'dec_trainset_owner = "cd_dataset.SimulatedBehavior"');
+
+% train all LC models
+parpopulate(cd_lc.TrainedLC, rel, 'lc_id in (3, 7)');
+
+parpopulate(cd_lc.TrainedLC, rel, 'lc_id between 1 and 7');
