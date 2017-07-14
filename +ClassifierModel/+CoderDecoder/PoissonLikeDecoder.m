680abf5c9c0dc90ee7af1f2b8e51c12897ef933b
Edgar Y. Walker <edgar.walker@gmail.com>
2017-07-14 13:15:28 -0500

    Update PoissonLike decoder

--- +ClassifierModel/+CoderDecoder/PoissonLikeDecoder.m
+++ +ClassifierModel/+CoderDecoder/PoissonLikeDecoder.m
@@ -2,35 +2,54 @@ classdef PoissonLikeDecoder < handle
     % POISSONLIKEDECODER Given spike data, returns can decode likelihood
     % function over the stimulus
     properties
-        binWidth; % binWidth for adj bin logistic regression
-        h; % Poisson like kernel
-        ori; % ori bins
+        nbins; % number of bins to divide stimulus domain into
+        w; % Poisson like kernel logistic weigths
+        bincc; % bin center for logistic fits
         unitFilter = ':'; % filter out bad units when computing likelihood distr
     end
     
     methods
-        function obj = PoissonLikeDecoder(binWidth)
+        function obj = PoissonLikeDecoder(nbins)
             % Constructer for PoissonLikeCodec
             if nargin > 0
-                obj.binWidth = binWidth;
+                obj.nbins = nbins;
             else
-                obj.binWidth = 5;
+                obj.nbins = 10;
             end
         end
         
-        function spikeCounts = encode(self, stimulus, contrast)
-            % ENCODE Takes in stimulus and contrast and encode
-            % probabilistic population coding
-            spikeCounts = self.baseEncoder.encode(stimulus, contrast); % obtains base deterministic population code
-            spikeCounts = poissrnd(spikeCounts); % add Poisson noise
-        end
+
         
         function train(self, dataSet)
-            [ori, h] = fitPoissonLike(self.binWidth, dataSet);
-            self.ori = ori;
-            self.h = h;
+
+            counts = dataSet.counts;
+            ori = dataSet.orientation;
+            n_prc = self.nbins;
+            edges = prctile(dataSet.orientation, linspace(0,100, n_prc));
+            n_labels = length(edges) - 1;
+            labels = sum(bsxfun(@le, edges(1:end-1), ori(:)), 2);
+            binc = 0.5 * (edges(1:end-1) + edges(2:end));
+            N = size(counts, 1);
+
+            w = zeros(N, n_labels - 1);
+            wo = w;
+            for i = 1:n_labels - 1
+                pos = labels == i | labels == i + 1;
+                r  = counts(:, pos);
+                lbl = labels(pos) == (i+1);
+                v = glmfit(r', lbl, 'binomial', 'link', 'logit', 'constant', 'off');
+                wo(:, i) = v;
+                w(:, i) = v / (binc(i+1) - binc(i));
+            end
+
+            bincc = 0.5 * (binc(1:end-1) + binc(2:end));
+            
+            self.w = w;
+            self.bincc = bincc;
+            
+
         end
-        
+
 %         function logLList = getLogLikelihood(self, stimulus, contrast, spikeCounts)
 %             % GETLOGLIKELIHOOD Evaluate the log likelihood of observing the 
 %             % spikeCounts for given stimulus for each units per trial
@@ -44,17 +63,18 @@ classdef PoissonLikeDecoder < handle
             % GETLIKELIHOODDISTR Calculates the likelihood distribution
             % over the range of orientation (decodeOri) for observing given
             % set of spikeCounts and contrast value for each trial
-            crudeLL = self.h' * spikeCounts;
-            logL = interp1(self.ori, crudeLL, decodeOri, 'cubic');
+            
+            deltaS = decodeOri(2) - decodeOri(1);
+
+            dh = interp1(self.bincc, self.w', decodeOri, 'PCHIP', 0);
+            h = cumsum(dh, 1) * deltaS;
+            
+            plot(decodeOri, h);
+            
+            logL = h * spikeCounts;
             normL = exp(bsxfun(@minus, logL, max(logL))); % max normalized likelihood
             normL(isnan(normL)) = 0;
             L = bsxfun(@rdivide, normL, sum(normL)); %likelihood function with normalized area
-
-%             L = interp1(self.ori, crudeL, decodeOri, 'cubic', NaN);
-%             L(isnan(L)) = 0;
-%             L = bsxfun(@rdivide, L, sum(L));
-            %L(L < 0) = 0;
-
         end
         
         
@@ -63,15 +83,15 @@ classdef PoissonLikeDecoder < handle
             % model. This includes ALL (fixed and non-fixed) parameters,
             % fix map, bounds, and model name
             configSet = [];
-            configSet.binWidth = self.binWidth;
-            configSet.ori = self.ori;
-            configSet.h = self.h;
+            configSet.nbins = self.nbins;
+            configSet.w = self.w;
+            configSet.bincc = self.bincc;
         end
         
         function setModelConfigs(self, configSet)
-            self.binWidth = configSet.binWidth;
-            self.ori = configSet.ori;
-            self.h = configSet.h;
+            self.nbins = configSet.nbins;
+            self.w = configSet.w;
+            self.bincc = configSet.bincc;
         end  
     end
 end
--- logisticFits.m
+++ logisticFits.m
@@ -28,3 +28,52 @@ plot(oric, h(19, :), 'r--');
 hold on;
 plot(decodeOri, y);
 xlim([220, 320]);
+
+%%
+counts = dataSet.counts;
+ori = dataSet.orientation;
+n_prc = 10;
+edges = prctile(dataSet.orientation, linspace(0,100, n_prc));
+n_labels = length(edges) - 1;
+labels = sum(bsxfun(@le, edges(1:end-1), ori(:)), 2);
+binc = 0.5 * (edges(1:end-1) + edges(2:end));
+
+w = zeros(96, n_labels - 1);
+wo = w;
+for i = 1:n_labels - 1
+    pos = labels == i | labels == i + 1;
+    r  = counts(:, pos);
+    lbl = labels(pos) == (i+1);
+    v = glmfit(r', lbl, 'binomial', 'link', 'logit', 'constant', 'off');
+    wo(:, i) = v;
+    w(:, i) = v / (binc(i+1) - binc(i));
+end
+
+bincc = 0.5 * (binc(1:end-1) + binc(2:end));
+
+x = linspace(250, 290, 200);
+deltaS = x(2) - x(1);
+
+dh = interp1(bincc, w', x, 'linear', 0);
+h = cumsum(dh, 1) * deltaS;
+%%
+idx= 53;
+
+figure;
+subplot(4,1,1);
+hold on;
+plot(binc, zeros(size(binc)), 'kx');
+plot(edges, zeros(size(edges)), 'rx');
+plot(bincc, wo(idx,:), 'o')
+subplot(4,1,2);
+plot(bincc, w(idx,:), 'o')
+subplot(4,1,3);
+plot(x, dh(:, idx));
+subplot(4,1,4);
+plot(x, h(:, idx));
+
+
+
+
+
+
