ae96dd417f3b3420f13f86528585776b96390f59
Edgar Y. Walker <edgar.walker@gmail.com>
2018-05-09 19:26:06 -0500

    Add shuffling fix for decoder_id = 1 case

--- utils/prepareDataSet.m
+++ utils/prepareDataSet.m
@@ -14,8 +14,19 @@ function dataSet=prepareDataSet(dataSet, decoder, key, nori)
 %     else
 %         decodeOri = linspace(220, 320, 1000);
 %     end
+    % :(  Dirty hack to restrict the dampening and renormalization only to
+    % decoder_id = 1 case (Likelihood based on tuning curves + poisson
+    % noise). Technically should be fine to apply to any other cases, but
+    % putting this restriction "just in case"
+    
     decodeOri = linspace(200, 340, nori);
     L = decoder.getLikelihoodDistr(decodeOri, dataSet.contrast, dataSet.counts);
+    if key.decoder_id == 1
+        damped = ones(length(decodeOri), 1);
+        damped(decodeOri < 230 | decodeOri > 310) = 0;
+        L = bsxfun(@times, damped, L);  % suppress responses outside of the reasonable domain
+        L = bsxfun(@rdivide, L, sum(L, 1)); % renormalize the area under the likelihood function
+    end
     dataSet.decodeOri = decodeOri;
     dataSet.likelihood = L;
     
