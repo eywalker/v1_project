db53b6942d5aa5016ab74589b0d99389e7f0db6a
Edgar Y. Walker <edgar.walker@gmail.com>
2018-05-22 22:28:03 -0500

    Train on decoder_id=4 (bin size 91)

--- recipes/DLSetFits.m
+++ recipes/DLSetFits.m
@@ -6,7 +6,3 @@ parpopulate(cd_dlset.DLSetInfo);
 restr = 'lc_id in (32, 38)';
 parpopulate(cd_dlset.TrainedLC, restr);
 parpopulate(cd_dlset.LCModelFits);
-
-% now fit on all the rest
-parpopulate(cd_dlset.TrainedLC);
-parpopulate(cd_dlset.LCModelFits);
