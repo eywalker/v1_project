a780776bd3f3a22f8ced45957448877a713ffc0f
Edgar Y. Walker <edgar.walker@gmail.com>
2019-02-19 14:14:00 +0100

    Update recipe for DL set fit

--- recipes/DLSetFits.m
+++ recipes/DLSetFits.m
@@ -2,6 +2,17 @@
 % first fit only on restricted subset
 restr = 'lc_id in (32, 38) and decoder_id=4';
 
+
+% get all contrast sessions
+parpopulate(cd_dataset.CleanContrastSessionDataSet);
+
+
+% register CleanContrastSessions as decoder trainset
+parpopulate(cd_decoder.DecoderTrainSets, 'dec_trainset_owner = "cd_dataset.CleanContrastSessionDataSet"');
+
+% use specialized filler table to populate decoder_id = 3 and 4 case
+parpopulate(cd_decoder.MLFiller);
+
 parpopulate(cd_dlset.DLSet);
 parpopulate(cd_dlset.DLSetInfo);
 
