c60292ec373078d791cc1a1918a2f0f904b7ac66
Edgar Y. Walker <edgar.walker@gmail.com>
2019-02-19 14:52:31 +0100

    Skip MLFiller

--- recipes/DLSetFits.m
+++ recipes/DLSetFits.m
@@ -13,7 +13,7 @@ parpopulate(cd_decoder.DecoderTrainSets, 'dec_trainset_owner = "cd_dataset.Clean
 parpopulate(cd_dlset.CVSet);
 
 % use specialized filler table to populate decoder_id = 4 case
-parpopulate(cd_decoder.MLFiller, 'decoder_id = 4');
+%parpopulate(cd_decoder.MLFiller, 'decoder_id = 4');
 
 parpopulate(cd_dlset.DLSet);
 %parpopulate(cd_dlset.DLSetInfo);
