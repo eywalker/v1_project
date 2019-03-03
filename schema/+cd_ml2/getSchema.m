bfe2ddf9bbd799ca4dc9b242a3fcc4965089a4a3
Edgar Y. Walker <edgar.walker@gmail.com>
2019-03-03 10:09:00 +0000

    Point to the updated schema

--- schema/+cd_ml2/getSchema.m
+++ schema/+cd_ml2/getSchema.m
@@ -1,7 +1,7 @@
 function obj = getSchema
 persistent schemaObject
 if isempty(schemaObject)
-    schemaObject = dj.Schema(dj.conn, 'cd_ml2', 'edgar_cd_ml2');
+    schemaObject = dj.Schema(dj.conn, 'cd_ml2', 'edgar_cd_ml2_updated');
 end
 obj = schemaObject;
 end
