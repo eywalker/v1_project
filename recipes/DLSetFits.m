1f9dd28d7f94a6a962d730fdfcc2289394b9bf7d
Edgar Y. Walker <edgar.walker@gmail.com>
2019-03-27 16:47:01 +0100

    Adjust the recipe

--- recipes/DLSetFits.m
+++ recipes/DLSetFits.m
@@ -34,29 +34,25 @@ parpopulate(cd_dlset.DLSetInfo, 'decoder_id in (11, 13, 15)');
 
 target_dset = pro(cd_dataset.CleanContrastSessionDataSet & (class_discrimination.CSCLookup & 'count_start = 0 and count_stop = 500'), 'dataset_hash -> dec_trainset_hash');
 
-restr = 'lc_id=32 and decoder_id in (13, 15, 5) and lc_shuffle_id=0 and sim_seed=12';
-parpopulate(cd_cross_sim.TrainedLC, target_dset, restr);
-parpopulate(cd_cross_sim.LCModelFits, target_dset, restr);
-
-restr = 'lc_id=32 and decoder_id in (13, 15, 5) and lc_shuffle_id=1 and sim_seed=12';
-parpopulate(cd_cross_sim.TrainedLC, target_dset, restr);
-parpopulate(cd_cross_sim.LCModelFits, target_dset, restr);
-
-restr = 'lc_id=32 and decoder_id in (13, 15, 5) and lc_shuffle_id=0 and sim_seed < 300';
-parpopulate(cd_cross_sim.TrainedLC, target_dset, restr);
-parpopulate(cd_cross_sim.LCModelFits, target_dset, restr);
-
-
-restr = 'lc_id=32 and decoder_id in (13, 15, 5) and lc_shuffle_id=0';
-parpopulate(cd_cross_sim.TrainedLC, target_dset, restr);
-parpopulate(cd_cross_sim.LCModelFits, target_dset, restr);
-
 
 % For comparison, do fit on 13 with model 38 (Gaussian approx of full)
 
 restr = 'lc_id=38 and decoder_id in (13) and lc_shuffle_id=0';
 parpopulate(cd_dlset.TrainedLC, restr);
 parpopulate(cd_dlset.LCModelFits, restr);
+
+
+restr = 'lc_id=32 and decoder_id in (4, 5) and lc_shuffle_id=0 and sim_seed < 300';
+parpopulate(cd_cross_sim.TrainedLC, target_dset, restr);
+parpopulate(cd_cross_sim.LCModelFits, target_dset, restr);
+
+restr = 'lc_id=32 and decoder_id in (4, 5) and lc_shuffle_id=1 and sim_seed < 300';
+parpopulate(cd_cross_sim.TrainedLC, target_dset, restr);
+parpopulate(cd_cross_sim.LCModelFits, target_dset, restr);
+
+
+
+
 
 % Fit now on shuffled dataset
 restr = 'lc_id=38 and decoder_id in (13) and lc_shuffle_id=1';
